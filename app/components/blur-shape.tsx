61755577cc0378117f9d0574998e26d3041a4203
Karl <karl@kejk.tech>
2023-11-04 23:29:26 +0000

    Refactor BlurShape component styles

--- app/components/blur-shape.tsx
+++ app/components/blur-shape.tsx
@@ -1,7 +1,7 @@
 export const BlurShape = () => {
     return (
-    <div className="relative grid h-[80%] w-[80%] place-items-center opacity-20 dark:opacity-10 lg:h-[750px] lg:w-[750px]">
-        <div className="bg-current/10 backdrop-blur-3xl lg:bg-transparent lg:blur-3xl">
+    <div className="relative grid h-[80%] w-[80%] place-items-center lg:opacity-20 lg:dark:opacity-10 lg:h-[750px] lg:w-[750px]">
+        <div className="bg-current/20 backdrop-blur-3xl lg:bg-transparent lg:blur-3xl">
         <div className="absolute left-0 top-0 w-24 h-24 md:h-96 md:w-96 rounded-full bg-lime-400" />
         <div className="absolute right-0 top-0 w-24 h-24 md:h-96 md:w-96 rounded-full bg-cyan-400" />
         <div className="absolute bottom-0 left-0 right-0 mx-auto w-24 h-24 md:h-96 md:w-96 rounded-full bg-indigo-400" />
