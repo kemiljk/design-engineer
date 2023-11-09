f63399277f1bcd5caa493f501523a9de637dfa08
Karl Koch <kkoch@duckduckgo.com>
2023-11-09 13:00:54 +0000

    Fix: remove unused export

--- app/api/stats/route.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-import { getStat, getStats } from "@/lib/cosmic";
-
-export async function GET() {
-  const stats = await getStats();
-  const statsArray = await getStat();
-  return { stats, statsArray };
-}
