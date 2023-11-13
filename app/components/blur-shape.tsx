ff104e529dafca86b6d85d9dcf21b5ec8d21da70
Karl <karl@kejk.tech>
2023-11-13 08:00:16 +0000

    Update BlurShape component

--- app/components/blur-shape.tsx
+++ app/components/blur-shape.tsx
@@ -2,9 +2,9 @@ export const BlurShape = () => {
     return (
     <div className="relative grid h-[300px] w-[300px] place-items-center opacity-20 lg:h-[750px] lg:w-[750px] blur-xl lg:blur-3xl">
         <div className="relative w-full h-full">
-        <div className="absolute left-0 top-0 w-32 h-32 md:h-96 md:w-96 rounded-full bg-lime-400" />
-        <div className="absolute right-0 top-0 w-32 h-32 md:h-96 md:w-96 rounded-full bg-cyan-400" />
-        <div className="absolute bottom-0 left-0 right-0 mx-auto w-32 h-32 md:h-96 md:w-96 rounded-full bg-indigo-400" />
+        <div className="absolute left-0 top-0 w-48 h-48 md:h-96 md:w-96 rounded-full bg-lime-400" />
+        <div className="absolute right-0 top-0 w-48 h-48 md:h-96 md:w-96 rounded-full bg-cyan-400" />
+        <div className="absolute bottom-0 left-0 right-0 mx-auto w-48 h-48 md:h-96 md:w-96 rounded-full bg-indigo-400" />
         </div>
       </div> 
     )
