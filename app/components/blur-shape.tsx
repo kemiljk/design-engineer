b85a175e454d1fc3cda0a5154805b8786b99f72c
Karl Koch <karl@kejk.tech>
2023-11-26 19:36:30 +0000

    feat: add motion to blur shape

--- app/components/blur-shape.tsx
+++ app/components/blur-shape.tsx
@@ -1,45 +1,72 @@
 "use client";
 
-import { motion, useReducedMotion } from "framer-motion";
+import { motion } from "framer-motion";
 
 export const BlurShape = () => {
-  const shouldReduceMotion = useReducedMotion();
+  const animation1 = {
+    animate: {
+      x: ["0px", "25px", "-25px", "-50px", "0px"],
+      y: ["0px", "50px", "-50px", "25px", "0px"],
+    },
+    transition: {
+      duration: 10,
+      ease: "easeInOut",
+      repeat: Infinity,
+    },
+  };
 
-  const animation = shouldReduceMotion
-    ? {}
-    : {
-        animate: {
-          x: ["0px", "10px", "-10px", "-5px", "0px"],
-          y: ["0px", "5px", "-5px", "10px", "0px"],
-        },
-        transition: {
-          duration: 5,
-          repeat: Infinity,
-        },
-      };
+  const animation2 = {
+    animate: {
+      x: ["0px", "-25px", "25px", "50px", "0px"],
+      y: ["0px", "-50px", "50px", "-25px", "0px"],
+    },
+    transition: {
+      duration: 11,
+      ease: "easeInOut",
+      repeat: Infinity,
+    },
+  };
+
+  const animation3 = {
+    animate: {
+      x: ["0px", "50px", "-50px", "-25px", "0px"],
+      y: ["0px", "25px", "-25px", "50px", "0px"],
+    },
+    transition: {
+      duration: 12,
+      ease: "easeInOut",
+      repeat: Infinity,
+    },
+  };
 
   return (
-    <div className="relative grid h-[300px] w-[300px] place-items-center opacity-20 blur-xl lg:h-[750px] lg:w-[750px] lg:blur-3xl">
+    <div className="relative grid h-[300px] w-[300px] place-items-center opacity-20 blur-xl md:h-[500px] md:w-[500px] lg:h-[750px] lg:w-[750px] lg:blur-3xl">
       <div className="relative h-full w-full">
         <motion.div
-          {...animation}
+          {...animation1}
           className={
-            "absolute left-0 top-0 h-48 w-48 rounded-full bg-lime-400 md:h-96 md:w-96"
+            "absolute left-0 top-0 h-48 w-48 rounded-full bg-lime-400 md:h-72 md:w-72 lg:h-[32rem] lg:w-[32rem]"
           }
         />
         <motion.div
-          {...animation}
+          {...animation2}
           className={
-            "absolute right-0 top-0 h-48 w-48 rounded-full bg-cyan-400 md:h-96 md:w-96"
+            "absolute right-0 top-0 h-48 w-48 rounded-full bg-cyan-400 md:h-72 md:w-72 lg:h-[32rem] lg:w-[32rem]"
           }
         />
         <motion.div
-          {...animation}
+          {...animation3}
           className={
-            "absolute bottom-0 left-0 right-0 mx-auto h-48 w-48 rounded-full bg-indigo-400 md:h-96 md:w-96"
+            "absolute bottom-0 right-0 h-48 w-48 rounded-full bg-purple-400 md:h-72 md:w-72 lg:h-[32rem] lg:w-[32rem]"
           }
         />
       </div>
+      <motion.div
+        {...animation2}
+        className={
+          "absolute bottom-0 left-0 h-48 w-48 rounded-full bg-blue-400 md:h-72 md:w-72 lg:h-96 lg:w-96"
+        }
+      />
     </div>
   );
 };
