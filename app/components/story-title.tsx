7a043e981bf8de9d50087fa893e39ea41bbf326b
Karl Koch <karl@kejk.tech>
2023-12-10 17:17:50 +0000

    Fix: better fonts, use scale to reduce not increase

--- app/components/story-title.tsx
+++ app/components/story-title.tsx
@@ -21,11 +21,15 @@ export const StoryTitle = ({ story }: { story: Type.Story }) => {
   }, []);
 
   return (
-    <div key={story.id} className="sticky top-3 z-[9999999] h-full" ref={ref}>
+    <div
+      key={story.id}
+      className="sticky top-3 z-[9999999] h-full md:top-1"
+      ref={ref}
+    >
       <motion.h1
-        className={`flex flex-col items-center gap-4 text-xl tracking-tight text-black dark:text-white md:text-3xl`}
+        className={`flex flex-col items-center text-3xl tracking-tight text-black dark:text-white md:text-5xl`}
         animate={{
-          scale: isSticky ? 1 : 1.5,
+          scale: isSticky ? 0.5 : 1,
           fontWeight: isSticky ? 700 : 800,
         }}
         transition={{ duration: 0.2 }}
