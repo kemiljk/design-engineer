25f671a0008970d60d701c3edfca76a3e7f2b8b3
Karl Koch <karl@kejk.tech>
2023-12-13 14:59:58 +0000

    fix: simpler route protection

--- middleware.ts
+++ middleware.ts
@@ -4,21 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: [
-    "/",
-    "/api/",
-    "/api/article-subnission",
-    "/api/receive-subnission",
-    "/api/send",
-    "/api/send-submission",
-    "/api/waitlist",
-    "/api/waitlisted",
-    "/about",
-    "/privacy-policy",
-    "/terms-of-service",
-    "/posts",
-    "/posts/[slug]",
-  ],
+  publicRoutes: (req) => !req.url.includes("/stats"),
 });
 
 export const config = {
