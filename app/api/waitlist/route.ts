37fae7d69b7b5a5a97c8e6ea74bc5eb71814a61c
kemiljk <karl@kejk.tech>
2024-01-29 15:02:11 +0000

    fix: add email to Waitlist audience automatically

--- app/api/waitlist/route.ts
+++ app/api/waitlist/route.ts
@@ -1,8 +1,22 @@
-import { type NextRequest } from "next/server";
 import { cosmic } from "@/lib/cosmic";
+import { type NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+const waitlist = "8687acca-4a77-4161-855a-c43b8e8fc38d";
 
 export async function POST(request: NextRequest) {
-  const res = await request.json();
-  const data = await cosmic.objects.insertOne(res.email);
-  return Response.json(data);
+  try {
+    const res = await request.json();
+    await cosmic.objects.insertOne(res.email);
+    const data = await resend.contacts.create({
+      email: res.email.title,
+      unsubscribed: false,
+      audience_id: waitlist,
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
 }
