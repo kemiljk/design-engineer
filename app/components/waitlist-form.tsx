cadd27c4d7176c9a88ec32ace85a6b7aeec25c6c
Karl <karl@kejk.tech>
2024-02-03 00:15:22 +0000

    Fix waitlist form component bug

--- app/components/waitlist-form.tsx
+++ app/components/waitlist-form.tsx
@@ -53,7 +53,7 @@ export const Form = () => {
   return (
     <div className="z-2 relative flex w-full max-w-xl flex-col gap-2 text-center">
       <form
-        className="flex h-16 w-full max-w-xl flex-col items-center justify-center gap-2 md:flex-row"
+        className="flex w-full max-w-xl flex-col items-center justify-center gap-2 md:flex-row"
         onSubmit={submit}
       >
         <Input
