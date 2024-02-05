96f890805a3502236d10591940ba31ff85e9537f
Karl <karl@kejk.tech>
2024-02-05 21:43:04 +0000

    Update className in Updates component

--- app/updates/page.tsx
+++ app/updates/page.tsx
@@ -42,7 +42,7 @@ const Updates = async () => {
             </div>
             <div
               className="prose dark:prose-invert prose-zinc"
-              dangerouslySetInnerHTML={{ __html: email?.data?.html ?? "" }}
+              dangerouslySetInnerHTML={{ __html: email?.data?.html.replace(/color: *rgb\(0 *, *0 *, *0 *\)/g, '') ?? "" }}
             />
           </>
         ))}
