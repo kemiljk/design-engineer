4d69ddea08fcc776366aa2a281913b741bd77f40
Karl Koch <karl@kejk.tech>
2024-05-03 12:48:07 +0100

    Fix: move comments to single page layout

---
+++ app/stories/[slug]/layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import CommentsPresence from "../../components/comments/comments-presence";
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <main>
+      <CommentsPresence roomId="stories-canvas">
+        <div className="mx-auto w-full p-4 md:px-16 lg:px-24">
+          <div className="flex w-full flex-col items-center md:mt-0">
+            <section>{children}</section>
+          </div>
+        </div>
+      </CommentsPresence>
+    </main>
+  );
+}
+
+export default Layout;
