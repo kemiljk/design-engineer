2e2430d85fb2ae856e8187a56faba9407ebef705
Karl Koch <karl@kejk.tech>
2024-05-09 10:51:06 +0100

    Fix: improvements to Task Builder with some presets to fill input

--- app/jobs/task-builder/page.tsx
+++ app/jobs/task-builder/page.tsx
@@ -4,14 +4,31 @@ import React, { useState, useEffect, useRef } from "react";
 import Markdown from "react-markdown";
 import SectionTitle from "@/app/components/section-title";
 import { StyledButton as Button } from "@/app/components/styled-button";
-import { CopyIcon, SparklesIcon } from "lucide-react";
+import { CopyIcon, EraserIcon, SendIcon, SparklesIcon } from "lucide-react";
 import { Message, continueConversation } from "./actions";
 import { readStreamableValue } from "ai/rsc";
-import { Divider, Input } from "@nextui-org/react";
+import { Card, CardBody, Divider, Input } from "@nextui-org/react";
+import { useMediaQuery } from "usehooks-ts";
 
 function TaskBuilder() {
   const [conversation, setConversation] = useState<Message[]>([]);
   const [input, setInput] = useState<string>("");
+  const isMobile = useMediaQuery("(max-width: 640px)");
+
+  const suggestions = [
+    {
+      text: "A Design Engineer, with experience designing for AI tools.",
+    },
+    {
+      text: "A UI Engineer, with experience designing for mobile applications.",
+    },
+    {
+      text: "A UX Engineer, with experience designing for e-commerce websites.",
+    },
+    {
+      text: "A Design Technologist, with experience designing for web applications.",
+    },
+  ];
 
   const completionRef = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
@@ -44,32 +61,20 @@ function TaskBuilder() {
   return (
     <div className="flex w-full max-w-3xl flex-col gap-4 pb-24">
       <SectionTitle>Task Builder</SectionTitle>
-      <span className="font-medium text-foreground-500">
-        Generate a task for a Design Engineer role at your company.
-      </span>
-      <div className="fixed bottom-0 z-20 flex w-full max-w-3xl gap-4 border-t border-foreground-100 bg-background px-4 py-4 *:sticky">
-        <Input
-          type="text"
-          value={input}
-          placeholder="Provide some basic task requirements..."
-          onChange={(event) => {
-            setInput(event.target.value);
-          }}
-          onKeyDown={(event) => {
-            if (event.key === "Enter") {
-              sendMessage();
-              setInput("");
-            }
-          }}
-        />
+      <div className="flex w-full items-baseline justify-between">
+        <span className="font-medium text-foreground-500">
+          Generate a task for a Design Engineer role at your company.
+        </span>
         <Button
-          variant="stylised"
-          onPress={sendMessage}
-          onClick={() => setInput("")}
-          className="mx-auto w-max bg-gradient-to-br from-secondary-400 to-primary-400 text-background disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:opacity-50"
-          startContent={<SparklesIcon className="shrink-0" size={16} />}
+          variant="flat"
+          className="w-max"
+          onPress={() => {
+            setConversation([]);
+            setInput("");
+          }}
+          startContent={<EraserIcon size={16} />}
         >
-          Generate
+          Clear chat
         </Button>
       </div>
       <div ref={completionRef} className="relative mt-12 max-w-3xl">
@@ -93,21 +98,75 @@ function TaskBuilder() {
                       }}
                     />
                   </div>
-                  {conversation.length > 1 && <Divider className="mt-8" />}
+                  {conversation.length > 2 && <Divider className="mb-4 mt-8" />}
                 </div>
               ),
           )
         ) : (
-          <div className="flex h-full w-full flex-col items-center justify-center gap-4">
-            <SparklesIcon
-              className="mx-auto mt-40 animate-pulse text-secondary-200"
-              size={104}
-            />
-            <span className="max-w-xs text-center text-foreground-500">
-              Start by entering some basic task requirements and click Generate.
-            </span>
+          <div className="m-auto flex h-full w-full flex-col items-center justify-center gap-4 md:min-h-[50dvh]">
+            <div className="flex h-full w-full flex-col">
+              <div className="flex h-full w-full flex-col gap-y-2">
+                <SparklesIcon
+                  className="mx-auto animate-pulse text-secondary-200"
+                  size={104}
+                />
+                <span className="mx-auto max-w-xs text-center text-foreground-500">
+                  Start by entering some basic task requirements and click
+                  Generate.
+                </span>
+              </div>
+              <div className="fixed bottom-20 left-0 right-0 grid w-full max-w-3xl grid-cols-1 gap-2 px-4 md:grid-cols-2 lg:left-auto lg:right-auto">
+                {suggestions.map((suggestion, index) => (
+                  <button
+                    key={index}
+                    className="w-full cursor-pointer select-none rounded-lg border border-foreground-200 px-4 py-2 text-start text-sm hover:bg-foreground-50 hover:shadow-sm"
+                    onClick={() => {
+                      setInput(suggestion.text);
+                    }}
+                  >
+                    <div className="flex w-full flex-col items-start gap-4 text-foreground-500">
+                      {suggestion.text}
+                    </div>
+                  </button>
+                ))}
+              </div>
+            </div>
           </div>
         )}
+      </div>
+      <div className="fixed bottom-0 left-0 right-0 z-20 flex w-full max-w-3xl gap-4 border-t border-foreground-100 bg-background px-4 py-4 *:sticky lg:left-auto lg:right-auto">
+        <Input
+          type="text"
+          value={input}
+          placeholder="Provide some basic task requirements..."
+          isClearable
+          onClear={() => setInput("")}
+          onChange={(event) => {
+            setInput(event.target.value);
+          }}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              sendMessage();
+              setInput("");
+            }
+          }}
+        />
+        <Button
+          variant="stylised"
+          onPress={sendMessage}
+          onClick={() => setInput("")}
+          className="mx-auto w-max bg-gradient-to-br from-secondary-400 to-primary-400 text-background disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:opacity-50"
+          startContent={
+            isMobile ? (
+              <SendIcon className="shrink-0" size={16} />
+            ) : (
+              <SparklesIcon className="shrink-0" size={16} />
+            )
+          }
+          isIconOnly={isMobile}
+        >
+          {!isMobile && "Generate"}
+        </Button>
       </div>
     </div>
   );
