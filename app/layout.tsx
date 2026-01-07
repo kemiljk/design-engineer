ba563d1898522e35db759c879dafe943923c28d3
kemiljk <karl@kejk.tech>
2026-01-07 15:10:23 +0000

    chore: Remove package-lock.json and update layout for smooth scrolling
    This commit deletes the `package-lock.json` file, which is no longer needed, and updates the `RootLayout` component to include a `data-scroll-behavior` attribute set to "smooth". This change aims to enhance the user experience by providing a smoother scrolling effect across the application.

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" data-scroll-behavior="smooth" suppressHydrationWarning>
       <head>
         <Script
           defer
